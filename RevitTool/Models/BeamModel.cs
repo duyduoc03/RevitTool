34c7ff2893c8c2b0080ffd1d356f49cf5ab102a2
duocld <duyduoc03bg@gmail.com>
2026-08-14 14:26:45 +0700

    14/08/2026 First

---
+++ RevitTool/Models/BeamModel.cs
@@ -0,0 +1,23 @@
+using Autodesk.Revit.DB;
+
+namespace RevitTool.Models
+{
+    public sealed class BeamModel : IElementModel
+    {
+        public required ElementId Id { get; init; }
+
+        public required string Name { get; init; }
+
+        public required string Level { get; init; }
+
+        public double Width { get; init; }
+
+        public double Height { get; init; }
+
+        public double Length { get; init; }
+
+        public string? Material { get; init; }
+
+        public string? Comments { get; init; }
+    }
+}
---
+++ RevitTool/Models/ColumnModel.cs
@@ -0,0 +1,23 @@
+using Autodesk.Revit.DB;
+
+namespace RevitTool.Models
+{
+    public class ColumnModel : IElementModel
+    {
+        public required ElementId Id { get; init; }
+
+        public required string Name { get; init; }
+
+        public required string Level { get; init; }
+
+        public double Width { get; init; }
+
+        public double Depth { get; init; }
+
+        public double Height { get; init; }
+
+        public string? Material { get; init; }
+
+        public string? Comments { get; init; }
+    }
+}
